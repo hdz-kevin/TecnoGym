dfaaf0fcbc8f87f1fcad4e3b7f888e3e479573b2
hdz-kevin <kevinhdez592@gmail.com>
2025-11-03 20:10:37 -0600

    add migration for payments table

--- app/Enums/MembershipStatus.php
+++ app/Enums/MembershipStatus.php
@@ -14,7 +14,7 @@ enum MembershipStatus: string
      */
     public static function values(): array
     {
-        return array_map(fn ($case) => $case->value, self::cases());
+        return array_map(fn (self $case) => $case->value, self::cases());
     }
 
     /**
---
+++ database/migrations/2025_11_03_193858_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enums\PaymentStatus;
+use App\Models\Membership;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Membership::class)->constrained()->onDelete('restrict');
+
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('price');
+            $table->enum('status', PaymentStatus::values())->default(PaymentStatus::IN_PROGRESS->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
