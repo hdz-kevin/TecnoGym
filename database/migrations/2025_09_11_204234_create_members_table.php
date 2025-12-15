4137789548b87bf280241eb1f3f2b1baf8adf523
hdz-kevin <kevinhdez592@gmail.com>
2025-12-15 15:55:57 -0600

    add status column on members table migration

--- database/migrations/2025_09_11_204234_create_members_table.php
+++ database/migrations/2025_09_11_204234_create_members_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Enums\MemberGender;
+use App\Enums\MemberStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,7 @@ return new class extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('status', MemberStatus::values())->default(MemberStatus::NO_MEMBERSHIP->value);
             $table->enum('gender', MemberGender::values());
             $table->date('birth_date')->nullable();
             $table->string('photo')->nullable();
