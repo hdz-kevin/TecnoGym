ba2bbb349f3bc9e6444f001d76d9f2f3383fee8a
hdz-kevin <kevinhdez592@gmail.com>
2026-01-14 17:43:24 -0600

    add migrations for visits tables

---
+++ database/migrations/2026_01_14_173353_create_visits_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\VisitType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(VisitType::class)->constrained()->onDelete('restrict');
+            $table->dateTime('visit_at');
+            $table->integer('price_paid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits');
+    }
+};
