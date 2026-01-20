c22f2ee97b2627645089b575ec852e9e837b7bec
hdz-kevin <kevinhdez592@gmail.com>
2026-01-20 15:33:52 -0600

    remove settings routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Livewire\Members;
 use App\Livewire\Memberships;
 use App\Livewire\Prices;
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
-use App\Livewire\Settings\Profile;
 use App\Livewire\VerifyMembership;
 use App\Livewire\Visits;
 use Illuminate\Support\Facades\Route;
@@ -13,16 +10,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => view('welcome'))->name('home');
 
 Route::view('dashboard', 'dashboard')
-     ->middleware(['auth', 'verified'])
+     ->middleware(['guest'])
      ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-
+Route::middleware(['guest'])->group(function () {
     Route::get('members', Members::class)->name('members.index');
     Route::get('prices', Prices::class)->name('prices.index');
     Route::get('memberships', Memberships::class)->name('memberships.index');
