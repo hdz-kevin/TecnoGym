75e03d41e8baf6ac9e0c3acca13d4b233c810795
hdz-kevin <kevinhdez592@gmail.com>
2026-01-30 18:51:28 -0600

    move flash message to the top

--- resources/views/livewire/members.blade.php
+++ resources/views/livewire/members.blade.php
@@ -371,7 +371,7 @@
   <!-- Flash Messages -->
   @if (session()->has('message'))
     <div
-      class="fixed text-lg font-medium bottom-8 right-8 bg-green-50 text-green-800 border border-green-400 px-6 py-2.5 rounded-lg shadow-lg z-50"
+      class="fixed font-medium top-5 right-5 bg-green-50 text-green-800 border border-green-400 px-6 py-2.5 rounded-lg shadow-lg z-50"
       x-data="{ show: true }"
       x-show="show"
       x-init="setTimeout(() => show = false, 3 * 1000)"
@@ -382,7 +382,7 @@
 
   @if (session()->has('error'))
     <div
-      class="fixed text-lg font-medium bottom-8 right-8 bg-red-50 text-red-800 border border-red-400 px-6 py-2.5 rounded-lg shadow-lg z-50"
+      class="fixed font-medium top-5 right-5 bg-red-50 text-red-800 border border-red-400 px-6 py-2.5 rounded-lg shadow-lg z-50"
       x-data="{ show: true }"
       x-show="show"
       x-init="setTimeout(() => show = false, 3 * 1000)"
