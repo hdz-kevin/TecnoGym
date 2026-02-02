0326decbb600a97f8ec099ed84d8c4aa1390ac24
hdz-kevin <kevinhdez592@gmail.com>
2026-02-02 17:29:49 -0600

    fix: ensure flash messages reappear on subsequent actions adding wire:key

--- resources/views/livewire/members.blade.php
+++ resources/views/livewire/members.blade.php
@@ -372,6 +372,7 @@
   @if (session()->has('message'))
     <div
       class="fixed font-medium top-5 right-5 bg-green-50 text-green-800 border border-green-300 px-6 py-2.5 rounded-lg shadow-lg z-50"
+      wire:key="{{ Str::random() }}"
       x-data="{ show: true }"
       x-show="show"
       x-init="setTimeout(() => show = false, 3 * 1000)"
@@ -383,6 +384,7 @@
   @if (session()->has('error'))
     <div
       class="fixed font-medium top-5 right-5 bg-red-50 text-red-800 border border-red-300 px-6 py-2.5 rounded-lg shadow-lg z-50"
+      wire:key="{{ Str::random() }}"
       x-data="{ show: true }"
       x-show="show"
       x-init="setTimeout(() => show = false, 3 * 1000)"
