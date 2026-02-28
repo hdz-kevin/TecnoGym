a562236af023908356538223f39a90f6b621e242
Tomas Hernandez <kevinhdez592@gmail.com>
2026-02-28 16:35:59 -0600

    feat: implement calculateEndDate method in Period model

--- app/Models/Period.php
+++ app/Models/Period.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Enums\DurationUnit;
 use App\Enums\PeriodStatus;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -53,5 +55,23 @@ class Period extends Model
         return $start->translatedFormat('d').' '.$startMonth.' '.$start->translatedFormat('Y')
                 .' - '.
                 $end->translatedFormat('d').' '.$endMonth.' '.$end->translatedFormat('Y');
+    }
+
+    /**
+     * Calculate the end date of a period based on the start date and period type.
+     *
+     * @param Carbon $startDate
+     * @param PeriodType $periodType
+     * @return Carbon
+     */
+    public static function calculateEndDate(Carbon $startDate, PeriodType $periodType): Carbon
+    {
+        $startDate = $startDate->copy();
+
+        return match ($periodType->duration_unit) {
+            DurationUnit::DAY => $startDate->addDays($periodType->duration_value),
+            DurationUnit::WEEK => $startDate->addWeeks($periodType->duration_value),
+            DurationUnit::MONTH => $startDate->addMonths($periodType->duration_value),
+        };
     }
 }
