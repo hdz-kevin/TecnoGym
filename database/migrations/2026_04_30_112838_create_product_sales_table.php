9c6ae17ac58d308b702821e8b9dacb5d889dd3f6
Kevin Hernandez <kevinhdez592@gmail.com>
2026-04-30 12:01:09 -0600

    feat (inventory): create tables for products and sales

---
+++ database/migrations/2026_04_30_112838_create_product_sales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Product;
+use App\Models\Sale;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Sale::class)->constrained()->onDelete('restrict');
+            $table->foreignIdFor(Product::class)->constrained()->onDelete('restrict');
+            $table->integer('quantity');
+            $table->decimal('unit_price');
+            $table->decimal('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_sales');
+    }
+};
